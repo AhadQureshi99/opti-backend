23eac20005f2affe9201eba934c4bda49b5735c4
Ahad Qureshi <ahadqureshi99@gmail.com>
2025-12-19 18:05:19 +0500

    "sales fixed"

--- models/Order.js
+++ models/Order.js
@@ -71,6 +71,7 @@ const orderSchema = new mongoose.Schema(
     user: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
+      required: true, // Ensures every order has an owner (main shop)
     },
   },
   {
